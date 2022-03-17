2ae21d25149f7a4f2b75c03b3b1cfea319d5f9d8
Lexx Luey <71068965+LexxLuey@users.noreply.github.com>
2022-03-17 11:57:30 +0100

    Remove Nav in form

--- resources/views/layouts/admin-dashboard.blade.php
+++ resources/views/layouts/admin-dashboard.blade.php
@@ -38,6 +38,43 @@
                 <a href="#" class="toggle-search"><i class="material-icons">close</i></a>
             </div>
             <div class="app-header">
+                <nav class="navbar navbar-light navbar-expand-lg">
+                    <div class="container-fluid">
+                        <div class="navbar-nav" id="navbarNav">
+
+                        </div>
+                        <div class="d-flex">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link toggle-search" href="#"><i class="material-icons">search</i></a>
+                                </li>
+
+                                <li class="nav-item hidden-on-mobile">
+                                    <a class="nav-link nav-notifications-toggle" id="notificationsDropDown" href="#"
+                                        data-bs-toggle="dropdown">
+                                        <div class="logo">
+                                            <div class="sidebar-user-switcher user-activity-online">
+                                                <img src="{{ asset("assets/images/avatars/avatar1.jpg") }}" height="45px" width="45px">
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
 
             <div class="app-content">
